bc952499c11fb12fcc76178ad81acbd2772cc802
John <tinco7@aol.com>
2020-03-05 17:12:11 -0800

    Initial Commit

---
+++ BadDecisionMaker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BadDecisionMaker
+//
+//  Created by CSUFTitan on 12/15/1398 AP.
+//  Copyright © 1398 CSUFTitan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
